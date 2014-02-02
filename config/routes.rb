09f60fbeb421dd2215bf93dea3d02a1e596ed764
oame <oame@oameya.com>
2014-02-02 18:45:02 +0900

    Add Active_Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Torapic::Application.routes.draw do
+  # Admin
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   root 'static_pages#index'
 
   resources :items, except: [:index] do
