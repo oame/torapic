df12a48dc30299b3e286e4dbecbeed764229fcd6
oame <oame@oameya.com>
2014-03-09 11:17:47 +0900

    U

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,17 +1,35 @@
-class PhotosController < InheritedResources::Base
+class PhotosController < ApplicationController
   include Pundit
 
   before_action :authenticate_user!
   before_action :set_photo, only: %i(show edit update destroy)
   before_action :authorize_photo, except: %i(show edit update destroy)
 
-  def index
-    @photos = Photos.all
+  def show
   end
 
   def new
     @photo = Photo.new
     render layout: 'layouts/landscape'
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+
+    if @photo.save
+      redirect_to @photo
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
   private
@@ -21,7 +39,7 @@ class PhotosController < InheritedResources::Base
   end
 
   def set_photo
-    @photo = Photo.find(photo_params)
+    @photo = Photo.find(params[:id])
   end
 
   def photo_params
--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20140307040016) do
   end
 
   add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
-  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
 
   create_table "admin_users", force: true do |t|
@@ -50,6 +49,7 @@ ActiveRecord::Schema.define(version: 20140307040016) do
     t.string   "image"
     t.integer  "user_id"
     t.datetime "expired_at"
+    t.datetime "deleted_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
