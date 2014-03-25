c2efe4f15ef0dec4b2342c89bd0b2ef8fb5b5c9b
oame <oame@oameya.com>
2014-03-25 14:22:40 +0900

    U

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,20 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      aws_access_key_id:     ENV["AWS_S3_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_S3_ACCESS_KEY"],
-      provider:              'AWS',
-      region:                'ap-northeast-1'
-    }
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:              'AWS',
+    aws_access_key_id:     ENV["AWS_S3_KEY"],
+    aws_secret_access_key: ENV["AWS_S3_SECRET"],
+    region:                'ap-northeast-1'
+  }
 
-    config.fog_directory = ENV["AWS_S3_BUCKET"]
-
-    config.cache_dir = "#{Rails.root}/tmp/uploads"
-  end
-else
-  CarrierWave.configure do |config|
+  if Rails.env.test? || Rails.env.development?
     config.storage = :file
+    config.root = "#{Rails.root}/tmp/uploads/#{DateTime.now.to_f}.#{rand(999)}.#{rand(999)}"
+  else
+    config.storage = :fog
   end
+
+  config.cache_dir = "#{Rails.root}/tmp/uploads/#{DateTime.now.to_f}.#{rand(999)}.#{rand(999)}"
+  config.fog_directory  = ENV['AWS_S3_BUCKET']
+  config.fog_public     = false
+  config.fog_attributes = {}
 end
