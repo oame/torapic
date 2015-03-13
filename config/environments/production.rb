813d5081b653e4730ad9f6cbbcdf5fe3ddc8dee4
oame <oame@oameya.com>
2015-03-14 00:07:59 +0900

    Update mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port:           ENV['MAILGUN_SMTP_PORT'],
     address:        ENV['MAILGUN_SMTP_SERVER'],
     user_name:      ENV['MAILGUN_SMTP_LOGIN'],
     password:       ENV['MAILGUN_SMTP_PASSWORD'],
-    domain:         'torapic.heroku.com',
+    domain:         'torapic.herokuapp.com',
     authentication: :plain,
   }
-  config.action_mailer.delivery_method = :smtp
 end
